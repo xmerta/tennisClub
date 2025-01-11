3446041c5153358db16fbc2a8b749dbf51a4e05a
Jakub Merta <xmerta@fi.muni.cz>
2025-01-11 16:08:25 +0100

    initial_setup

---
+++ src/main/java/cz/xmerta/tennisclub/TennisClubApplication.java
@@ -0,0 +1,13 @@
+package cz.xmerta.tennisclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TennisClubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/xmerta/tennisclub/TennisClubApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.xmerta.tennisclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
